8109f0b52fb47728f1de59814969bc5cc6200e94
Josh Comeau <joshwcomeau@gmail.com>
2014-10-03 19:20:32 -0400

    woot. Working mix filering, using ControllerAs syntax, with "this" instead of hanging everything on the scope. Deviated from a tutorial and it worked!

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,14 +7,50 @@ blueberry = angular.module('blueberry', [
 blueberry.config(['$routeProvider', function($routeProvider) {
   $routeProvider
   .when('/', {
-    templateUrl: 'index.html',
-    controller:  'MixesController'
+    templateUrl:  'index.html',
+    controller:   'MixesController',
+    controllerAs: 'mixes'
   });
 }]);
 
-controllers = angular.module('controllers', [])
-controllers.controller("RecipesController", RecipesController)
+function MixesController($scope, $routeParams, $location) {
+  this.search = function(keywords) {
+    $location.path("/").search('keywords', this.keywords);
+  }
 
-function RecipesController() {
-  
+  console.log($routeParams);
+
+  if ( $routeParams.keywords ) {
+    search_term = $routeParams.keywords.toLowerCase();
+    this.selectedMixes = mixlist.filter(function(mix) {
+      return mix.name.toLowerCase().indexOf(search_term) != -1
+    });
+    console.log(this.selectedMixes);
+  } else {
+    this.selectedMixes = []
+  }
 }
+
+MixesController.$inject = ['$scope', '$routeParams', '$location']
+
+controllers = angular.module('controllers', [])
+controllers.controller("MixesController", ['$scope', '$routeParams', '$location', MixesController])
+
+
+mixlist = [
+  {
+    id: 1,
+    name: 'Supersonic Overdrive',
+    description: 'An uplifting mix of hard trance, hard dance and hard drives.'
+  },
+  {
+    id: 2,
+    name: 'ChilZone',
+    description: 'A downtempo stroll through a dreamy park.'
+  },
+  {
+    id: 3,
+    name: 'Pot Pourri',
+    description: 'There\'s something for everyone in this wild, all-over-the-place mix.'
+  }
+]
